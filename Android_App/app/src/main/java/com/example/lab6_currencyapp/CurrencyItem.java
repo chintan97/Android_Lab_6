4e72d9fb66bd6f51bd1d2e36bb8c5720e131bd12
Chintan Patel <chintan97patel@gmail.com>
2019-02-28 16:23:21 -0400

    Completed App

---
+++ Android_App/app/src/main/java/com/example/lab6_currencyapp/CurrencyItem.java
@@ -0,0 +1,19 @@
+package com.example.lab6_currencyapp;
+
+public class CurrencyItem {
+    private String base;
+    private double value;
+
+    public CurrencyItem(String base, double value){
+        this.base = base;
+        this.value = value;
+    }
+
+    public String getBase(){
+        return this.base;
+    }
+
+    public double getValue(){
+        return this.value;
+    }
+}
